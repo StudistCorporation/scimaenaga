c38d6dc24c385de4040a90de7adc06f2df9a3759
Motoshi Takeda <motoshi.takeda@studist.jp>
2022-02-01 17:48:31 +0900

    change class structure

---
+++ app/libraries/scim_patch_operation_group.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+class ScimPatchOperationGroup < ScimPatchOperation
+
+  def save(model)
+    if @path_scim == 'members' # Only members are supported for value is an array
+      update_member_ids = @value.map do |v|
+        v[ScimRails.config.group_member_relation_schema.keys.first].to_s
+      end
+
+      current_member_ids = model.public_send(
+        ScimRails.config.group_member_relation_attribute
+      ).map(&:to_s)
+      case @op
+      when 'add'
+        member_ids = current_member_ids.concat(update_member_ids)
+      when 'replace'
+        member_ids = current_member_ids.concat(update_member_ids)
+      when 'remove'
+        member_ids = current_member_ids - update_member_ids
+      end
+
+      # Only the member addition process is saved by each ids
+      model.public_send("#{ScimRails.config.group_member_relation_attribute}=",
+                        member_ids.uniq)
+      return
+    end
+
+    case @op
+    when 'add', 'replace'
+      model.attributes = { @path_sp => @value }
+    when 'remove'
+      model.attributes = { @path_sp => nil }
+    end
+  end
+
+  private
+
+    def operation_mutable_attributes_schema
+      ScimRails.config.mutable_group_attributes_schema
+    end
+
+    def validate(_op, _path, _value)
+      return
+    end
+
+    def convert_path(_path)
+      # TODO: When feature flag is specified,
+      #       Azure AD sends member remove request as follows:
+      # "Operations": [
+      #   {
+      #       "op": "remove",
+      #       "path": "members[value eq \"7f4bc1a3-285e-48ae-8202-5accb43efb0e\"]"
+      #   }
+      # ]
+      #
+      # This format will have to be supported.
+      mutable_attributes_schema.dig(*dig_keys)
+    end
+
+end
---
+++ app/libraries/scim_patch_operation_user.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class ScimPatchOperationUser < ScimPatchOperation
+
+  def save(model)
+    case @op
+    when 'add', 'replace'
+      model.attributes = { @path_sp => @value }
+    when 'remove'
+      model.attributes = { @path_sp => nil }
+    end
+  end
+
+  private
+
+    def operation_mutable_attributes_schema
+      ScimRails.config.mutable_user_attributes_schema
+    end
+
+    def validate(_op, _path, value)
+      if value.instance_of? Array
+        raise ScimRails::ExceptionHandler::UnsupportedPatchRequest
+      end
+
+      return
+    end
+
+    def convert_path(path)
+      # For now, library does not support Multi-Valued Attributes properly.
+      # examle:
+      #   path = 'emails[type eq "work"].value'
+      #   mutable_attributes_schema = {
+      #     emails: [
+      #       {
+      #         value: :mail_address,
+      #      }
+      #     ],
+      #   }
+      #
+      #   Library ignores filter conditions (like [type eq "work"])
+      #   and always uses the first element of the array
+      dig_keys = path.gsub(/\[(.+?)\]/, '.0').split('.').map do |step|
+        step == '0' ? 0 : step.to_sym
+      end
+      mutable_attributes_schema.dig(*dig_keys)
+    end
+
+end
