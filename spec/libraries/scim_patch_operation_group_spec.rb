69010f3e81db0871d49eed41d4ed260b5f903d2c
Motoshi Takeda <motoshi.takeda@studist.jp>
2022-02-03 20:11:40 +0900

    accept member remove request by using filter

--- spec/libraries/scim_patch_operation_group_spec.rb
+++ spec/libraries/scim_patch_operation_group_spec.rb
@@ -9,6 +9,12 @@ describe ScimPatchOperationGroup do
   let(:mutable_attributes_schema) { { displayName: :name } }
   let(:group_member_relation_attribute) { :user_ids }
 
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+  let(:user3) { create(:user) }
+  let(:user4) { create(:user) }
+  let(:group) { create(:group, users: [user1, user2]) }
+
   let(:operation) do
     described_class.new(
       op,
@@ -17,7 +23,7 @@ describe ScimPatchOperationGroup do
     )
   end
   describe '#initialize' do
-    context 'replace single attribute' do
+    context 'replace displayName' do
       it {
         allow(ScimRails.config).to(
           receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
@@ -29,10 +35,13 @@ describe ScimPatchOperationGroup do
         expect(operation.path_scim).to eq(attribute: path, rest_path: [])
         expect(operation.path_sp).to eq :name
         expect(operation.value).to eq value
+
+        operation.save(group)
+        expect(group.name).to eq value
       }
     end
 
-    context 'add single attribute' do
+    context 'add displayName' do
       let(:op) { 'add' }
       it {
         allow(ScimRails.config).to(
@@ -45,10 +54,13 @@ describe ScimPatchOperationGroup do
         expect(operation.path_scim).to eq(attribute: path, rest_path: [])
         expect(operation.path_sp).to eq :name
         expect(operation.value).to eq value
+
+        operation.save(group)
+        expect(group.name).to eq value
       }
     end
 
-    context 'remove single attribute' do
+    context 'remove displayName' do
       let(:op) { 'remove' }
       it {
         allow(ScimRails.config).to(
@@ -61,13 +73,16 @@ describe ScimPatchOperationGroup do
         expect(operation.path_scim).to eq(attribute: path, rest_path: [])
         expect(operation.path_sp).to eq :name
         expect(operation.value).to eq value
+
+        operation.save(group)
+        expect(group.name).to eq nil
       }
     end
 
-    context 'add user ids' do
+    context 'add member' do
       let(:op) { 'add' }
       let(:path) { 'members' }
-      let(:value) { [{ 'value' => '1' }, { 'value' => '2' }] }
+      let(:value) { [{ 'value' => user3.id.to_s }, { 'value' => user4.id.to_s }] }
       it {
         allow(ScimRails.config).to(
           receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
@@ -79,13 +94,37 @@ describe ScimPatchOperationGroup do
         expect(operation.path_scim).to eq(attribute: 'members', rest_path: [])
         expect(operation.path_sp).to eq :user_ids
         expect(operation.value).to eq value
+
+        operation.save(group)
+        expect(group.users).to eq [user1, user2, user3, user4]
+      }
+    end
+
+    context 'replace member' do
+      let(:op) { 'replace' }
+      let(:path) { 'members' }
+      let(:value) { [{ 'value' => user3.id.to_s }, { 'value' => user4.id.to_s }] }
+      it {
+        allow(ScimRails.config).to(
+          receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
+        )
+        allow(ScimRails.config).to(
+          receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
+        )
+        expect(operation.op).to eq 'replace'
+        expect(operation.path_scim).to eq(attribute: 'members', rest_path: [])
+        expect(operation.path_sp).to eq :user_ids
+        expect(operation.value).to eq value
+
+        operation.save(group)
+        expect(group.users).to eq [user3, user4]
       }
     end
 
     context 'remove user ids' do
       let(:op) { 'remove' }
       let(:path) { 'members' }
-      let(:value) { [{ 'value' => '1' }, { 'value' => '2' }] }
+      let(:value) { [{ 'value' => user1.id.to_s }, { 'value' => user2.id.to_s }] }
       it {
         allow(ScimRails.config).to(
           receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
@@ -96,13 +135,16 @@ describe ScimPatchOperationGroup do
         expect(operation.op).to eq 'remove'
         expect(operation.path_scim).to eq(attribute: 'members', rest_path: [])
         expect(operation.path_sp).to eq :user_ids
-        expect(operation.value).to eq [{ 'value' => '1' }, { 'value' => '2' }]
+        expect(operation.value).to eq value
+
+        operation.save(group)
+        expect(group.users).to eq []
       }
     end
 
     context 'remove user id (with filter)' do
       let(:op) { 'remove' }
-      let(:path) { 'members[value eq "abcdef01"]' }
+      let(:path) { "members[value eq \"#{user1.id}\"]" }
       let(:value) { nil }
       it {
         allow(ScimRails.config).to(
@@ -113,9 +155,12 @@ describe ScimPatchOperationGroup do
         )
         expect(operation.op).to eq 'remove'
         expect(operation.path_scim).to eq(attribute: 'members',
-                                          filter: { attribute: 'value', operator: 'eq', parameter: 'abcdef01' }, rest_path: [])
+                                          filter: { attribute: 'value', operator: 'eq', parameter: user1.id.to_s }, rest_path: [])
         expect(operation.path_sp).to eq :user_ids
         expect(operation.value).to eq nil
+
+        operation.save(group)
+        expect(group.users).to eq [user2]
       }
     end
   end
