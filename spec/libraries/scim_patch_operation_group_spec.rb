6b50126c1479734f9985722d58f36903d60569a9
Motoshi Takeda <motoshi.takeda@studist.jp>
2022-02-04 15:00:57 +0900

    remove redundant describe

--- spec/libraries/scim_patch_operation_group_spec.rb
+++ spec/libraries/scim_patch_operation_group_spec.rb
@@ -22,146 +22,144 @@ describe ScimPatchOperationGroup do
       value
     )
   end
-  describe '#initialize' do
-    context 'replace displayName' do
-      it {
-        allow(ScimRails.config).to(
-          receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
-        )
-        allow(ScimRails.config).to(
-          receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
-        )
-        expect(operation.op).to eq 'replace'
-        expect(operation.path_scim).to eq(attribute: path, rest_path: [])
-        expect(operation.path_sp).to eq :name
-        expect(operation.value).to eq value
+  context 'replace displayName' do
+    it {
+      allow(ScimRails.config).to(
+        receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
+      )
+      allow(ScimRails.config).to(
+        receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
+      )
+      expect(operation.op).to eq 'replace'
+      expect(operation.path_scim).to eq(attribute: path, rest_path: [])
+      expect(operation.path_sp).to eq :name
+      expect(operation.value).to eq value
 
-        operation.save(group)
-        expect(group.name).to eq value
-      }
-    end
+      operation.save(group)
+      expect(group.name).to eq value
+    }
+  end
 
-    context 'add displayName' do
-      let(:op) { 'add' }
-      it {
-        allow(ScimRails.config).to(
-          receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
-        )
-        allow(ScimRails.config).to(
-          receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
-        )
-        expect(operation.op).to eq 'add'
-        expect(operation.path_scim).to eq(attribute: path, rest_path: [])
-        expect(operation.path_sp).to eq :name
-        expect(operation.value).to eq value
+  context 'add displayName' do
+    let(:op) { 'add' }
+    it {
+      allow(ScimRails.config).to(
+        receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
+      )
+      allow(ScimRails.config).to(
+        receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
+      )
+      expect(operation.op).to eq 'add'
+      expect(operation.path_scim).to eq(attribute: path, rest_path: [])
+      expect(operation.path_sp).to eq :name
+      expect(operation.value).to eq value
 
-        operation.save(group)
-        expect(group.name).to eq value
-      }
-    end
+      operation.save(group)
+      expect(group.name).to eq value
+    }
+  end
 
-    context 'remove displayName' do
-      let(:op) { 'remove' }
-      it {
-        allow(ScimRails.config).to(
-          receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
-        )
-        allow(ScimRails.config).to(
-          receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
-        )
-        expect(operation.op).to eq 'remove'
-        expect(operation.path_scim).to eq(attribute: path, rest_path: [])
-        expect(operation.path_sp).to eq :name
-        expect(operation.value).to eq value
+  context 'remove displayName' do
+    let(:op) { 'remove' }
+    it {
+      allow(ScimRails.config).to(
+        receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
+      )
+      allow(ScimRails.config).to(
+        receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
+      )
+      expect(operation.op).to eq 'remove'
+      expect(operation.path_scim).to eq(attribute: path, rest_path: [])
+      expect(operation.path_sp).to eq :name
+      expect(operation.value).to eq value
 
-        operation.save(group)
-        expect(group.name).to eq nil
-      }
-    end
+      operation.save(group)
+      expect(group.name).to eq nil
+    }
+  end
 
-    context 'add member' do
-      let(:op) { 'add' }
-      let(:path) { 'members' }
-      let(:value) { [{ 'value' => user3.id.to_s }, { 'value' => user4.id.to_s }] }
-      it {
-        allow(ScimRails.config).to(
-          receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
-        )
-        allow(ScimRails.config).to(
-          receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
-        )
-        expect(operation.op).to eq 'add'
-        expect(operation.path_scim).to eq(attribute: 'members', rest_path: [])
-        expect(operation.path_sp).to eq :user_ids
-        expect(operation.value).to eq value
+  context 'add member' do
+    let(:op) { 'add' }
+    let(:path) { 'members' }
+    let(:value) { [{ 'value' => user3.id.to_s }, { 'value' => user4.id.to_s }] }
+    it {
+      allow(ScimRails.config).to(
+        receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
+      )
+      allow(ScimRails.config).to(
+        receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
+      )
+      expect(operation.op).to eq 'add'
+      expect(operation.path_scim).to eq(attribute: 'members', rest_path: [])
+      expect(operation.path_sp).to eq :user_ids
+      expect(operation.value).to eq value
 
-        operation.save(group)
-        expect(group.users).to eq [user1, user2, user3, user4]
-      }
-    end
+      operation.save(group)
+      expect(group.users).to eq [user1, user2, user3, user4]
+    }
+  end
 
-    context 'replace member' do
-      let(:op) { 'replace' }
-      let(:path) { 'members' }
-      let(:value) { [{ 'value' => user3.id.to_s }, { 'value' => user4.id.to_s }] }
-      it {
-        allow(ScimRails.config).to(
-          receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
-        )
-        allow(ScimRails.config).to(
-          receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
-        )
-        expect(operation.op).to eq 'replace'
-        expect(operation.path_scim).to eq(attribute: 'members', rest_path: [])
-        expect(operation.path_sp).to eq :user_ids
-        expect(operation.value).to eq value
+  context 'replace member' do
+    let(:op) { 'replace' }
+    let(:path) { 'members' }
+    let(:value) { [{ 'value' => user3.id.to_s }, { 'value' => user4.id.to_s }] }
+    it {
+      allow(ScimRails.config).to(
+        receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
+      )
+      allow(ScimRails.config).to(
+        receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
+      )
+      expect(operation.op).to eq 'replace'
+      expect(operation.path_scim).to eq(attribute: 'members', rest_path: [])
+      expect(operation.path_sp).to eq :user_ids
+      expect(operation.value).to eq value
 
-        operation.save(group)
-        expect(group.users).to eq [user3, user4]
-      }
-    end
+      operation.save(group)
+      expect(group.users).to eq [user3, user4]
+    }
+  end
 
-    context 'remove user ids' do
-      let(:op) { 'remove' }
-      let(:path) { 'members' }
-      let(:value) { [{ 'value' => user1.id.to_s }, { 'value' => user2.id.to_s }] }
-      it {
-        allow(ScimRails.config).to(
-          receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
-        )
-        allow(ScimRails.config).to(
-          receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
-        )
-        expect(operation.op).to eq 'remove'
-        expect(operation.path_scim).to eq(attribute: 'members', rest_path: [])
-        expect(operation.path_sp).to eq :user_ids
-        expect(operation.value).to eq value
+  context 'remove user ids' do
+    let(:op) { 'remove' }
+    let(:path) { 'members' }
+    let(:value) { [{ 'value' => user1.id.to_s }, { 'value' => user2.id.to_s }] }
+    it {
+      allow(ScimRails.config).to(
+        receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
+      )
+      allow(ScimRails.config).to(
+        receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
+      )
+      expect(operation.op).to eq 'remove'
+      expect(operation.path_scim).to eq(attribute: 'members', rest_path: [])
+      expect(operation.path_sp).to eq :user_ids
+      expect(operation.value).to eq value
 
-        operation.save(group)
-        expect(group.users).to eq []
-      }
-    end
+      operation.save(group)
+      expect(group.users).to eq []
+    }
+  end
 
-    context 'remove user id (with filter)' do
-      let(:op) { 'remove' }
-      let(:path) { "members[value eq \"#{user1.id}\"]" }
-      let(:value) { nil }
-      it {
-        allow(ScimRails.config).to(
-          receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
-        )
-        allow(ScimRails.config).to(
-          receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
-        )
-        expect(operation.op).to eq 'remove'
-        expect(operation.path_scim).to eq(attribute: 'members',
-                                          filter: { attribute: 'value', operator: 'eq', parameter: user1.id.to_s }, rest_path: [])
-        expect(operation.path_sp).to eq :user_ids
-        expect(operation.value).to eq nil
+  context 'remove user id (with filter)' do
+    let(:op) { 'remove' }
+    let(:path) { "members[value eq \"#{user1.id}\"]" }
+    let(:value) { nil }
+    it {
+      allow(ScimRails.config).to(
+        receive(:mutable_group_attributes_schema).and_return(mutable_attributes_schema)
+      )
+      allow(ScimRails.config).to(
+        receive(:group_member_relation_attribute).and_return(group_member_relation_attribute)
+      )
+      expect(operation.op).to eq 'remove'
+      expect(operation.path_scim).to eq(attribute: 'members',
+                                        filter: { attribute: 'value', operator: 'eq', parameter: user1.id.to_s }, rest_path: [])
+      expect(operation.path_sp).to eq :user_ids
+      expect(operation.value).to eq nil
 
-        operation.save(group)
-        expect(group.users).to eq [user2]
-      }
-    end
+      operation.save(group)
+      expect(group.users).to eq [user2]
+    }
   end
 end
